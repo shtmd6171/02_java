233223529f71dc7c1b613cbaf45e8e525d8d92d6
shtmd6171 <samanth.n.morton@gmail.com>
2019-07-04 10:45:27 +0900

    첫 작성

---
+++ day_001/src/hello/Hello.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int ad;
+		System.out.println("motherfucker");
+		ad = sc.nextInt();
+		System.out.println(ad);
+	}
+
+}
