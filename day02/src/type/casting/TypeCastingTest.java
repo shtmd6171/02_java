734e6be1b99d6b9175095bb6a8e110908a28a04b
shtmd6171 <samanth.n.morton@gmail.com>
2019-07-08 08:50:39 +0900

    -

--- day02/src/type/casting/TypeCastingTest.java
+++ day02/src/type/casting/TypeCastingTest.java
@@ -1,24 +1,37 @@
 package type.casting;
 
+/**
+ * 숫자형 데이터 사이에
+ * 형 변환(Type Casting)을 
+ * 테스트하는 클래스이다.
+ * 
+ * @author 304
+ *
+ */
 public class TypeCastingTest {
 
-	/*
-	 * 숫자형 데이터 사이에
-	 * 형 변환을 테스트하는 클래스
-	 */
-	
 	public static void main(String[] args) {
-		
+		// 1. 선언, 2. 초기화
 		double pi = 3.1415926535797;
-		int num = 100;
-		int result = num + (int)pi;
-		System.out.println("pi : "+pi);
-		System.out.println("num :"+num);
-		System.out.println("result :"+result);
+		int number = 100;
+		int result;
 		
-		byte buffer = (byte)result;
-		System.out.println("result :"+result);
-		System.out.println("byte :"+buffer);
+		// 3. 형 변환 : (타입이름) 피연산값;
+		result = number + (int) pi;
+		
+		// 4. 사용 : 값 출력
+		System.out.println("double pi=" + pi);
+		System.out.println("int number=" + number);
+		System.out.println("int result=" + result);
+		System.out.println("=================================");
+		
+		// 5. byte 타입으로 result 를 변환 저장
+		byte buffer = (byte) result;
+		System.out.println("int result=" + result);
+		System.out.println("byte buffer=" + buffer);
 	}
 
 }
+
+
+
--- day_001/src/hello/Hello.java
+++ day_001/src/hello/Hello.java
@@ -1,23 +1,25 @@
 package hello;
 
-import java.util.Scanner;
-import java.util.*;
-import java.time.*;
-
 public class Hello {
 
 	public static void main(String[] args) {
+		// 자바 기본 주석 
+		// 로 시작한다.
+		// 프로그램 실행에 영향을 미치지 않음.
 		
-		Scanner sc = new Scanner(System.in);
+		// 자바는 대소문자가 틀리면 구문오류인 경우가
+		// 많으므로 대소문자 오타주의 하세요.
+		System.out.println("Hello, World!");
 		
-		String line;
-		System.out.print("input : ");
+		// 실행 : ctrl + F11 
+		
+		// 자바 기본 출력 구문
+		// sysout -> ctrl + space bar
+		System.out.println("안녕하세요, 자바!");
+		System.out.println("안녕하세요, 자바!");
 
-		line = sc.nextLine();
-		System.out.println(line.toUpperCase());
-		// sysout +ctrl + spacebar
-		// shift + ctrl + 3 = staged 
-		
+		// 소스 편집 후 항상 저장하는 습관
+		// ctrl + s 단축키로 항상 저장
 	}
 
 }
