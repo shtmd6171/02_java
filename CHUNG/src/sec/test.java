0ddeb953bb8252974364df30688e8d40f150b415
shtmd6171 <samantha.n.morton@gmail.com>
2020-07-23 16:10:38 +0900

    classpath

---
+++ CHUNG/src/sec/test.java
@@ -0,0 +1,28 @@
+package sec;
+
+import java.util.ArrayList;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+
+	
+		
+		/*
+		 * int value = 5; for(int i=0; i<=value; i++) { for(int j=0; j<value-i; j++) {
+		 * System.out.print(""); } for(int k=0; k<2*value-i; k++) {
+		 * System.out.print("*"); } System.out.println(); }
+		 * 
+		 * for (int i = 0; i < value; i++) { for (int j = 1; j < value - i; j++) {
+		 * System.out.print(" "); } for (int k = 0; k < i * 2 + 1; k++) {
+		 * System.out.print("*"); } System.out.println(); } System.out.println(); for
+		 * (int i = value; i > 0; i--) { for (int j = value - i; j > 0; j--) {
+		 * System.out.print(" "); } for (int k = 0; k < i * 2 - 1; k++) {
+		 * System.out.print("*"); } System.out.println(); }
+		 */
+		
+		
+	}
+}
+
